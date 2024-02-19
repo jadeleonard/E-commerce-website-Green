cd387f4528dfdc2aa2d0d18e825786e4a689e1ab
jadeleonard <lukabartolomeo9@gmail.com>
2024-02-18 16:59:18 -0800

    added client

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3001
+const {PrismaClient} = require("@prisma/client")
 
 
 
 
 
-
+const prisma = new PrismaClient();
 app.listen(port,(res,req) =>{
     console.log(`Server running on port ${port}`)
+});
+app.get("/api/navbar",async(res,req) =>{
+    try {
+        const response = await prisma.navbar.findMany();
+        if(response){
+            res.json(response)
+        }
+    } catch (error) {
+        console.log("error",error)
+    }
 });
