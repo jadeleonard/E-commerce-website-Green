184f48ff2ff03763539bbc997cd2cbdc8351f8c7
jadeleonard <lukabartolomeo9@gmail.com>
2024-02-18 20:57:50 -0800

    added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.DATA_SEND;
+const port = process.env.DATA_URL;
 const { PrismaClient } = require("@prisma/client");
 const cache = require('memory-cache');
 
